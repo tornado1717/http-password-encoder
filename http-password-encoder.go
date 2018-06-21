839ca37c41b6224cec551c10664b0910bfa855f6
Paul Steinbrecher <tornado1717@gmail.com>
2018-06-21 13:08:18 -0600

    Improved logging output formats.

--- http-password-encoder.go
+++ http-password-encoder.go
@@ -15,26 +15,34 @@ import (
 	"flag"
 	"net/http"
 	"net/url"
-	"time"
-	"os"
 	"context"
+	"math/rand"
 )
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Global Constants
+// Globals
 ///////////////////////////////////////////////////////////////////////////////
 
 const (
-	pcol = "\x1B[32m"
-	hcol = "\x1B[33m"
-	rcol = "\x1B[0m"
+	pcol = "\x1B[32m"  // makes text dark green on a terminal
+	hcol = "\x1B[33m"  // makes text dark yellow on a terminal
+	rcol = "\x1B[0m"   // reset text colors
 )
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Funcs
 ///////////////////////////////////////////////////////////////////////////////
+
+func generateGoRoutineIDTag() string {
+	fgColorCode := rand.Intn(8)
+	bgColorCode := rand.Intn(7)
+	if fgColorCode <= bgColorCode {
+		bgColorCode++
+	}
+	return fmt.Sprint("\x1B[", fgColorCode+30, ";", bgColorCode+40, "m##", rcol)
+}
 
 // This handles requests that weren't sent according to the project spec -
 // browsers vs various curl parameters vs whatever other clients
@@ -58,71 +66,70 @@ func parseURLParams(req *http.Request, bodyData *[]byte) (url.Values, error) {
 }
 
 func printRequest(indent string, req *http.Request, bodyData *[]byte) {
-	fmt.Print(indent,       "req.Method='"    , req.Method    ,       "'\n")
-	fmt.Print(indent,       "req.URL.*\n")
-	fmt.Print(indent,       "    *.Scheme='"        , req.URL.Scheme      ,       "'\n")
-	fmt.Print(indent,       "    *.Opaque='"        , req.URL.Opaque      ,       "'\n")
-	fmt.Print(indent,       "    *.User='"          , req.URL.User        ,       "'\n")
-	fmt.Print(indent,       "    *.Host='"          , req.URL.Host        ,       "'\n")
-	fmt.Print(indent, hcol, "    *.Path='"          , req.URL.Path        , rcol, "'\n")
-	fmt.Print(indent,       "    *.RawPath='"       , req.URL.RawPath     ,       "'\n")
-	fmt.Print(indent,       "    *.ForceQuery='"    , req.URL.ForceQuery  ,       "'\n")
-	fmt.Print(indent, hcol, "    *.RawQuery='"      , req.URL.RawQuery    , rcol, "'\n")
-	fmt.Print(indent,       "    *.Fragment='"      , req.URL.Fragment    ,       "'\n")
-	fmt.Print(indent,       "req.Proto='"           , req.Proto           ,       "'\n")
-	fmt.Print(indent,       "req.ProtoMajor='"      , req.ProtoMajor      ,       "'\n")
-	fmt.Print(indent,       "req.ProtoMinor='"      , req.ProtoMinor      ,       "'\n")
-	fmt.Print(indent,       "req.Header='"          , req.Header          ,       "'\n")
-	fmt.Print(indent,       "req.Body='"            , req.Body            ,       "'\n")
-	fmt.Print(indent,       "    -> '"              ,         bodyData    ,       "'\n")
-	fmt.Print(indent,       "    -> '"              , string(*bodyData)   ,       "'\n")
-//	fmt.Print(indent,       "req.GetBody()='"       , req.GetBody()       ,       "'\n")
-	fmt.Print(indent,       "req.ContentLength='"   , req.ContentLength   ,       "'\n")
-	fmt.Print(indent,       "req.TransferEncoding='", req.TransferEncoding,       "'\n")
-	fmt.Print(indent,       "req.Close='"           , req.Close           ,       "'\n")
-	fmt.Print(indent,       "req.Host='"            , req.Host            ,       "'\n")
-	fmt.Print(indent,       "req.Host='"            , req.Host            ,       "'\n")
-	fmt.Print(indent,       "<before req.ParseForm()>:\n")
-	fmt.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
-	fmt.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
+	log.Print(indent,       "req.Method='"    , req.Method    ,       "'\n")
+	log.Print(indent,       "req.URL.*\n")
+	log.Print(indent,       "    *.Scheme='"        , req.URL.Scheme      ,       "'\n")
+	log.Print(indent,       "    *.Opaque='"        , req.URL.Opaque      ,       "'\n")
+	log.Print(indent,       "    *.User='"          , req.URL.User        ,       "'\n")
+	log.Print(indent,       "    *.Host='"          , req.URL.Host        ,       "'\n")
+	log.Print(indent, hcol, "    *.Path='"          , req.URL.Path        , rcol, "'\n")
+	log.Print(indent,       "    *.RawPath='"       , req.URL.RawPath     ,       "'\n")
+	log.Print(indent,       "    *.ForceQuery='"    , req.URL.ForceQuery  ,       "'\n")
+	log.Print(indent, hcol, "    *.RawQuery='"      , req.URL.RawQuery    , rcol, "'\n")
+	log.Print(indent,       "    *.Fragment='"      , req.URL.Fragment    ,       "'\n")
+	log.Print(indent,       "req.Proto='"           , req.Proto           ,       "'\n")
+	log.Print(indent,       "req.ProtoMajor='"      , req.ProtoMajor      ,       "'\n")
+	log.Print(indent,       "req.ProtoMinor='"      , req.ProtoMinor      ,       "'\n")
+	log.Print(indent,       "req.Header='"          , req.Header          ,       "'\n")
+	log.Print(indent,       "req.Body='"            , req.Body            ,       "'\n")
+	log.Print(indent,       "    -> '"              ,         bodyData    ,       "'\n")
+	log.Print(indent,       "    -> '"              , string(*bodyData)   ,       "'\n")
+//	log.Print(indent,       "req.GetBody()='"       , req.GetBody()       ,       "'\n")
+	log.Print(indent,       "req.ContentLength='"   , req.ContentLength   ,       "'\n")
+	log.Print(indent,       "req.TransferEncoding='", req.TransferEncoding,       "'\n")
+	log.Print(indent,       "req.Close='"           , req.Close           ,       "'\n")
+	log.Print(indent,       "req.Host='"            , req.Host            ,       "'\n")
+	log.Print(indent,       "req.Host='"            , req.Host            ,       "'\n")
+	log.Print(indent,       "<before req.ParseForm()>:\n")
+	log.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
+	log.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
+// TODO: look at req.FormValue() and req.FormPostValue()
 	req.ParseForm()  // It seems that this only reads the URL parameters and NOT params that were thrown into the body
-	fmt.Print(indent,       "<after req.ParseForm()>:\n")
-	fmt.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
-	fmt.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
+	log.Print(indent,       "<after req.ParseForm()>:\n")
+	log.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
+	log.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
 	req.ParseMultipartForm(0x1000)
-	fmt.Print(indent,       "<after req.ParseMultipartForm()>:\n")
-	fmt.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
-	fmt.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
-	fmt.Print(indent,       "req.MultipartForm='"   , req.MultipartForm   ,       "'\n")
-	fmt.Print(indent,       "req.Trailer='"         , req.Trailer         ,       "'\n")
-	fmt.Print(indent,       "req.RemoteAddr='"      , req.RemoteAddr      ,       "'\n")
-	fmt.Print(indent, hcol, "req.RequestURI='"      , req.RequestURI      , rcol, "'\n")
-	fmt.Print(indent,       "req.TLS='"             , req.TLS             ,       "'\n")
-	fmt.Print(indent,       "req.Cancel='"          , req.Cancel          ,       "'\n")
-//	fmt.Print(indent,       "req.Response='"        , req.Response        ,       "'\n")
+	log.Print(indent,       "<after req.ParseMultipartForm()>:\n")
+	log.Print(indent,       "    req.Form='"            , req.Form            ,       "'\n")
+	log.Print(indent,       "    req.PostForm='"        , req.PostForm        ,       "'\n")
+	log.Print(indent,       "req.MultipartForm='"   , req.MultipartForm   ,       "'\n")
+	log.Print(indent,       "req.Trailer='"         , req.Trailer         ,       "'\n")
+	log.Print(indent,       "req.RemoteAddr='"      , req.RemoteAddr      ,       "'\n")
+	log.Print(indent, hcol, "req.RequestURI='"      , req.RequestURI      , rcol, "'\n")
+	log.Print(indent,       "req.TLS='"             , req.TLS             ,       "'\n")
+	log.Print(indent,       "req.Cancel='"          , req.Cancel          ,       "'\n")
+//	log.Print(indent,       "req.Response='"        , req.Response        ,       "'\n")
 //	b, err := ioutil.ReadAll(Response.Body)
 //	if err != nil {
 //		log.Fatal(err)
 //	}
-//	fmt.Printf("%s", b)
+//	log.Printf("%s", b)
 
-	fmt.Println()
+	log.Println()
 	queryVals, err := parseURLParams(req, bodyData)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(indent,       "extracted query params:",       "\n")
-	fmt.Print(indent,       "    error: ", err, "\n")
-	fmt.Print(indent, hcol, "    queryVals: ", queryVals, rcol, "\n")
+	log.Print(indent,       "extracted query params:",       "\n")
+	log.Print(indent,       "    error: ", err, "\n")
+	log.Print(indent, hcol, "    queryVals: ", queryVals, rcol, "\n")
 }
 
 // Handle a few things all requests have in common:
 //	* Parse any parameters given in the URL (and search the body for them if some client puts them in there for some reason).
 //	* Retrieve all data from req.Body since it is an io.ReadCloser which is strictly read-once.  Data is placed into bodyData.
 //	* Some general logging statements.
-func processRequestCommon(w http.ResponseWriter, req *http.Request, callerNameTag string, bodyData *[]byte) {
-	startTime := time.Now()
-
+func processRequestCommon(w http.ResponseWriter, req *http.Request, callerNameTag, logTag string, bodyData *[]byte) {
 	io.WriteString(w, fmt.Sprint("Message from the ", callerNameTag, " request handler via io.WriteString\n"))
 	fmt.Fprint    (w,            "Message from the ", callerNameTag, " request handler via fmt.Fprint\n")
 
@@ -132,41 +139,43 @@ func processRequestCommon(w http.ResponseWriter, req *http.Request, callerNameTa
 	*bodyData, err = ioutil.ReadAll(req.Body)
 	if err != nil {
 		// TODO: what if this doesn't return EOF
-		log.Fatal(err)
+		log.Fatal(fmt.Sprint(logTag, err))
 	}
 	req.Body.Close()  // the server (this) is responsible for doing this
 
-	logTag := fmt.Sprintf("==%d:0xXXXXXXXX== %s %s",
-		os.Getpid(),
-		//os.GetGoRoutineID() or os.GetThreadID()  // Doesn't exist. See: https://github.com/golang/go/issues/22770
-		startTime.Format("2006-01-02 15:04:05.000000000"),
-		callerNameTag,
-	)
-	fmt.Print(pcol, callerNameTag, " handler:", rcol, "\n")
-	printRequest(logTag+"    : ", req, bodyData)
+	log.Print(logTag, pcol, " ", callerNameTag, " handler:", rcol, "\n")
+	printRequest(logTag + "    : ", req, bodyData)
 
-	fmt.Println(logTag+"    <sleeping>")
+/*	log.Println(logTag, "    <sleeping>")
 	time.Sleep(5 * time.Second)
-	fmt.Println(logTag+"    <done sleeping>")
+	log.Println(logTag, "    <done sleeping>") */
 }
 
 func handleGeneralRequest(w http.ResponseWriter, req *http.Request) {
+	callerNameTag := "general"
+	logTag := generateGoRoutineIDTag() + " " + callerNameTag
 	var bodyData []byte
-	processRequestCommon(w, req, "general", &bodyData)
+	processRequestCommon(w, req, "general", logTag, &bodyData)
 }
 
 func handleHashRequest_rootOnly(w http.ResponseWriter, req *http.Request) {
+	callerNameTag := "root-only hash"
+	logTag := generateGoRoutineIDTag() + " " + callerNameTag
+
 	var bodyData []byte
-	processRequestCommon(w, req, "root-only hash", &bodyData)
+	processRequestCommon(w, req, callerNameTag, logTag, &bodyData)
 }
 
 func handleHashRequest(w http.ResponseWriter, req *http.Request) {
+	callerNameTag := "hash"
+	logTag := generateGoRoutineIDTag() + " " + callerNameTag
+
 	var bodyData []byte
-	processRequestCommon(w, req, "hash", &bodyData)
+	processRequestCommon(w, req, callerNameTag, logTag, &bodyData)
 }
 
 func handleIgnoredRequest(w http.ResponseWriter, req *http.Request) {
-	fmt.Print(pcol, "handler was called for a request that is being ignored..., req.URL.Path=", req.URL.Path, rcol, "\n")
+	log.Print(pcol, "handler was called for a request that is being ignored..., req.URL.Path=", req.URL.Path, rcol, "\n")
 }
 
 
@@ -175,12 +184,14 @@ func handleIgnoredRequest(w http.ResponseWriter, req *http.Request) {
 ///////////////////////////////////////////////////////////////////////////////
 
 func main() {
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+
 	var serverPort = flag.String("port",
 		"8080",
 		"Port number for the HTTP password encoder server to use",
 	)
 	flag.Parse()
-	fmt.Print("Creating server on port ", *serverPort, "\n")
+	log.Print("Creating server on port ", *serverPort, "\n")
 
 	http.HandleFunc("/favicon.ico", handleIgnoredRequest)
 	http.HandleFunc("/hash/", handleHashRequest)
@@ -205,7 +216,7 @@ func main() {
 	}()
 
 	<- shutdownRequested
-	fmt.Println("Shutting down server...")
+	log.Println("Shutting down server...")
 	if err := server.Shutdown(context.Background()); err != nil {
 		// Error from closing listeners, or context timeout:
 		log.Printf("HTTP server Shutdown: %v", err)
